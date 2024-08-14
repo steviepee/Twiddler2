a9f5bf8796da8de501d9a327af5b9e014745eadd
Stefan <jimisax@gmail.com>
2024-08-14 09:03:17 -0500

    final before final

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ $(document).ready(() => {
   const $body = $('body');
   $body.html('');//clears the body
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//....................................................................................................................................................//
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////  TWIDSPACE!   //////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-//TWEET DIV STYLE
 
 $($body).css('background-color', 'purple')
 $body.append("<div id='titular'></div><div id='twidSpace'></div>")
@@ -26,11 +24,32 @@ $($twidSpace).css( 'padding', '25px 25px 25px 25px');
 $($twidSpace).css( 'margin', 'auto')
 $($twidSpace).css( 'width', '500px')
 
-////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+///////////////////  TWID IT UP STYLER  /////////////////////
+/////////////////////////////////////////////////////////////
 
- //TITLE STYLE
+
+$body.prepend('<div id="twid-up"><div id="twidButt"></div><button id="twid-more">Twid it up!</button></div>')
+
+let $twidMore = ('#twid-more')
+
+$($twidMore).css( 'background-color', 'blue')
+$($twidMore).css( 'color', 'yellow')
+$($twidMore).css( 'padding', '10px 5px')
+$($twidMore).css( 'font-size', '24px')
+$($twidMore).css( 'border-radius', '40px')
+$($twidMore).css( 'font-family', 'comic sans')
+$($twidMore).css( 'position', 'relative')
+$($twidMore).css( 'width', '500px')
+$($twidMore).css( 'padding', '25px 25px 25px 25px')
+
+$("#twid-up").css( 'display', 'flex')
+$("#twid-up").css( 'align-items', 'center')
+$("#twid-up").css( 'justify-content', 'center')
+
+
+
  let $title = $("<div><h1 class='site-title'>TWIDDLER</h1><h3 class='twid-title'>Get Cha Twid on!</h3><div>")
 
 
@@ -49,68 +68,39 @@ $($twidSpace).css( 'width', '500px')
  $("h3").css('top', '66%')
  $("h3").css('left', '48%')
 
-////////////////////////////////////////////////////////////
-
-/////////////////////////////////////////////////////////////
-
-
-//MORE TWEETS BUTTON
-
-$body.append('<div id="twid-up"><div id="twidButt"></div><button id="twid-more">Twid it up!</button></div>')
-
-let $twidMore = ('#twid-more')
-
-$($twidMore).css( 'background-color', 'blue')
-$($twidMore).css( 'color', 'yellow')
-$($twidMore).css( 'padding', '10px 5px')
-$($twidMore).css( 'font-size', '24px')
-$($twidMore).css( 'border-radius', '40px')
-$($twidMore).css( 'font-family', 'comic sans')
-$($twidMore).css( 'position', 'relative')
-$($twidMore).css( 'width', '500px')
-$($twidMore).css( 'padding', '25px 25px 25px 25px')
-
-$("#twid-up").css( 'display', 'flex')
-$("#twid-up").css( 'align-items', 'center')
-$("#twid-up").css( 'justify-content', 'center')
 
 ////////////////////////////////////////////////////////////
-
+//////////////////  TWID BUTTON ////////////////////////////
 ////////////////////////////////////////////////////////////
 
- //MAKE A  TWID BUTTON
 
   $twiddleButt = $("");
 
   $('#twid-more').click(function(){
     $twidSpace.append(makeTwids())
-    $($twidSpace).animate({scrollTop: $($twidSpace).prop('scrollHeight')}, 100);
+    $($twidSpace).animate({scrollTop: $($twidSpace).prop('scrollHeight')}, 50);
   })
 
 
-////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////
-
-//Twid Input Styling
 
 $body.append('<div id="myTwid"></div>')
 
 $myTwid = $('#myTwid');
 
-$myTwid.append("<form id='twidAdd'><input type='text/ ><div class='butt-spacer'></div><button type='button' id='twiddle'>Twiddle your fingers</button></form>")
+$myTwid.append("<form id='twidAdd'><input type='text' id= 'speak' value= ''/ ><div class='butt-spacer'></div><button type='button' id='twiddle'>Twiddle your fingers</button></form>")
 
 $($myTwid).css( 'display', 'flex')
 $($myTwid).css( 'align-items', 'center')
 $($myTwid).css( 'justify-content', 'center')
-$($myTwid).css( 'height', '100px')
+$($myTwid).css( 'height', '250px')
 
 $('#twidAdd').css( 'padding', '12px 20px');
-$('#twidAdd').css( 'margin', '80px 0');
+$('#twidAdd').css( 'margin', '8px 0');
 $('#twidAdd').css( 'width', '500px');
-$('#twidAdd').css( 'align-items','center')
+//$('#twidAdd').css( 'align-items','center')
 
-//$('#speak').css('width', '50%')
+$('#speak').css('width', '50%')
 
 $('#twiddle').css('width', '50%')
 $('#twiddle').css( 'background-color', 'maroon')
@@ -119,18 +109,19 @@ $('#twiddle').css( 'color', 'gold')
 $('#twiddle').css( 'font-family', 'comic sans')
 $('#twiddle').css( 'font-size', '25px')
 $('#twiddle').css( 'border-radius', '40px')
+$('#twiddle').css( 'margin', '20')
 
 $('#twid-up').prepend($("<div class='butt-spacer'></div>"))
 
 $("#twidButt").css('margin-top', '15%')
 
-$('.butt-spacer').css('height', '4px')
+$('.butt-spacer').css('height', '10px')
 
 $('#twiddle').css
 $('#twiddle').css
 
 ////////////////////////////////////////////////////////////
-
+///////////////////  MAKE TWIDS  ///////////////////////////
 ////////////////////////////////////////////////////////////
 
 $(document).ready(function(){
@@ -139,12 +130,6 @@ $(document).ready(function(){
        // Submit the form
   });
 });
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//....................................................................................................................................................//
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
- //FUNCTION MAKE TWIDS
 
 
   function makeTwids(){
@@ -156,7 +141,7 @@ $(document).ready(function(){
 
       $twid.append($twidUser)
       $twid.append("<div class='twid-mess'>" + twid.message +"</div>");
-      const $twidTime = $("<div class='twid-time'>" +"<div class='time-since'> - - - just now . . . . . . . . " + moment().calendar() + "</div>")
+      const $twidTime = $("<div class='twid-time'>" +"<div class='time-since'> - - - just now - - - - - - - - - - - " + moment().calendar() + "</div>")
 
       const $thisMoment = $("<div class='this-moment'>"+ moment().format() + "</div>").hide()
    
@@ -172,23 +157,33 @@ $(document).ready(function(){
     $twidSpace.append($twids)
     $($twidSpace)
     $singleTwid = $('.single-twid');
-    $('.username').css('color', 'yellow');
-    $('.username').css('font-size', '15px')
+
+
+
+
+    $('.username').css( 'color', 'Blue');
+    $('.username').css( 'font-size', '30px')
+
+    $('.twid-mess').css( 'color', 'gold');
+    $('.twid-mess').css( 'font-size', '25px');
+
+    $('.twid-time').css( 'font-size', '20px')
+    $('.twid-time').css( 'color', 'white')
     const $userName = $(".username")
 
 
 ////////////////////////////////////////////////////////////
-//........................................................//
+//////////////////  SEE PROFILES  //////////////////////////
 ////////////////////////////////////////////////////////////
 
-//CLICK ON OTHER USERNAME
 $($userName).on('click', function(){
 
 currentUser = this;
  
-  $("#title-spacer").append('<div id="user-profile">')
-  $('#user-profile').prepend('<button id="close-butt">X</button>')
+  $("#titular").append('<div id="user-profile"></div>')
+  $('#user-profile').append(`<button id="close-butt">Back t'wit!</button>`)
   $('#twidSpace').hide()
+  
 
   $("#twid-more").prop(
     "disabled",
@@ -201,13 +196,13 @@ $("#twiddle").prop(
   true
 );
 
-// X CLOSE BUTTON STYLE
-  $("#close-butt").css( 'background-color', '#555555')
+/*********************************  STYLES  ******************************/
+  $("#close-butt").css( 'background-color', 'blue')
   //  $("#close-butt").css( 'padding', '10px 10px')
-  $("#close-butt").css( 'font-size', '12px')
-  $("#close-butt").css( 'color', 'white')
+  $("#close-butt").css( 'font-size', '20px')
+  $("#close-butt").css( 'color', 'gold')
   $("#close-butt").css( 'border-radius', '4px')
-  $("#close-butt").css( 'font-family', 'cursive')
+  $("#close-butt").css( 'font-family', 'comic sans')
   $("#close-butt").css( 'position', 'fixed')
   $("#close-butt").css( 'top', '100px')
   $("#close-butt").css( 'right', '67%')
@@ -234,14 +229,14 @@ $("#twiddle").prop(
 ///////////////////////////////////////
   //SELECTED USER'S PROFILE STYLING
   // console.log("!!! User Profile !!!", $('#user-profile'))
-  $('#user-profile').css( 'background-color', '#F5F5F5')
+  $('#user-profile').css( 'background-color', 'purple')
   $('#user-profile').css( 'border', '1px solid #DDDDDD')
   $('#user-profile').css( 'border-radius', '4px 4px 4px 4px')
   $('#user-profile').css( 'font-size', '12px')
   $('#user-profile').css( 'font-weight', 'bold')
   $('#user-profile').css( 'padding', '10px 7px 5px')
   $('#user-profile').css( 'display', 'block')
-  $('#user-profile').css( 'height', '200px')
+  $('#user-profile').css( 'height', '300px')
   $('#user-profile').css( 'overflow', 'scroll');
   $('#user-profile').css( 'padding', '25px 25px 25px 25px');
   $('#user-profile').css( 'margin', 'auto')
@@ -262,19 +257,19 @@ $("#twiddle").prop(
 
       let twidArr = $fullTwid.split(":")
       let choppedUserName = twidArr.shift()
-      let choppedTime = twidArr[1].split("2024")
+      let choppedTime = twidArr[1].split("")
       
       
       $fullTwid = twidArr[0] + ":" + choppedTime[0]
       
       $("#user-profile")
       .append($("<div id='chop-user'><h2>" + choppedUserName + "</h2></div>")
-      .css('color', 'gold'))
+      .css('color', 'blue'))
       .append("<p>")
-      //.append($fullTwid)
+      .append($fullTwid)
       .append(twidArr[0] + ":" + choppedTime[0])
       .append("</p>")
-      .append("<div> - - - - - - </div>")
+      .append("<div> - - - - </div>")
 
       
 
@@ -310,7 +305,7 @@ $myTwid.append($twiddleButt)
       const $inputString = $('#speak').val();
       const $dudeTwidder = $("<div class='youser-name'>" +"<h2> @YOU:</h2></div>")
       const $userTwid = $("<div class='your-string'>"+ $inputString + " " + "</div>")
-      const $twidTime = $("<div class='twid-time'>" + (moment().startOf('hour').fromNow()) + " . . . . . . . . " + moment().calendar() + "</div>")
+      const $twidTime = $("<div class='twid-time'>" +"<div class='time-since'> - - - just now - - - - - - - - - - - " + moment().calendar() + "</div>")
       const $thisMoment = $("<div class='this-moment'>"+ moment().format() + "</div>").hide()
       $('#twidSpace').append($dudeTwidder)
       .append($userTwid)
@@ -320,9 +315,12 @@ $myTwid.append($twiddleButt)
       // .append(moment().calendar())
       // .append(" - - - ", moment().format());
 
-      $('.youser-name').css('color', 'orange');
+      $('.youser-name').css('color', 'green');
 
       $($twidSpace).animate({scrollTop: $($twidSpace).prop('scrollHeight')}, 100);
+
+      $('.your-string').css('font-size', '25px')
+      $('.your-string').css('color', 'blue')
     });
   };
 makeOwnTwids();
@@ -336,10 +334,9 @@ makeOwnTwids();
 //YOUR OWN PROFILE 
 
  $($("#twidSpace")).on('click', '.youser-name' ,function(){
-   console.log("you clicked")
   
-   $("#twidSpace").append('<div id="user-profile">')
-   $('#user-profile').prepend('<button id="close-butt">X</button>')
+   $("#titular").append('<div id="user-profile">')
+   $('#user-profile').prepend(`<button id="close-butt">Back t'wid</button>`)
    $('#twidSpace').hide()
 
 
@@ -360,24 +357,22 @@ makeOwnTwids();
    true
 );
 ////////////////////////////////////////////////////////
-
+//////////////   BACK TWID  ////////////////////////////
 ////////////////////////////////////////////////////////
-//YOUR OWN TWID HISTORY
 
-// X CLOSE BUTTON STYLE
-$("#close-butt").css( 'background-color', '#555555')
-//  $("#close-butt").css( 'padding', '10px 10px')
-$("#close-butt").css( 'font-size', '12px')
-$("#close-butt").css( 'color', 'white')
-$("#close-butt").css( 'border-radius', '4px')
-$("#close-butt").css( 'font-family', 'cursive')
-$("#close-butt").css( 'position', 'fixed')
-$("#close-butt").css( 'top', '100px')
-$("#close-butt").css( 'right', '67%')
-$("#close-butt").css( 'transform', 'translate(-50%, -50%')
-$("#close-butt").css( 'margin', 'auto')
-//  $("#close-butt").css( 'width', '5%')
-//  $("#close-butt").css( 'padding', '8px 8px 8px 8px')
+$("#close-butt").css( 'background-color', 'blue')
+  //  $("#close-butt").css( 'padding', '10px 10px')
+  $("#close-butt").css( 'font-size', '20px')
+  $("#close-butt").css( 'color', 'gold')
+  $("#close-butt").css( 'border-radius', '4px')
+  $("#close-butt").css( 'font-family', 'comic sans')
+  $("#close-butt").css( 'position', 'fixed')
+  $("#close-butt").css( 'top', '100px')
+  $("#close-butt").css( 'right', '67%')
+  $("#close-butt").css( 'transform', 'translate(-50%, -50%')
+   $("#close-butt").css( 'margin', 'auto')
+  //  $("#close-butt").css( 'width', '5%')
+  //  $("#close-butt").css( 'padding', '8px 8px 8px 8px')
 
 
 //closes the profile window
@@ -398,14 +393,14 @@ $('#close-butt').on('click', function(){
 
 ///////////////////////////////////////
 //YOUR OWN USER PROFILE STYLING
-$('#user-profile').css( 'background-color', '#F5F5F5')
+$('#user-profile').css( 'background-color', 'purple')
 $('#user-profile').css( 'border', '1px solid #DDDDDD')
 $('#user-profile').css( 'border-radius', '4px 4px 4px 4px')
-$('#user-profile').css( 'font-size', '12px')
+$('#user-profile').css( 'font-size', '20px')
 $('#user-profile').css( 'font-weight', 'bold')
 $('#user-profile').css( 'padding', '10px 7px 5px')
 $('#user-profile').css( 'display', 'block')
-$('#user-profile').css( 'height', '200px')
+$('#user-profile').css( 'height', '300px')
 $('#user-profile').css( 'overflow', 'scroll');
 $('#user-profile').css( 'padding', '25px 25px 25px 25px');
 $('#user-profile').css( 'margin', 'auto')
@@ -431,7 +426,7 @@ $("#user-profile").prepend($($userName))
 for (let i = 0; i < $userName.length; i++){
   $($userName[i]).append($($twidClone[i])).append($($timeClone)[i])
 }
-$($twidClone).css("color", "black")
+$($twidClone).css("color", "yellow")
 $($timeClone).css("color", "black")
 
 
@@ -469,15 +464,15 @@ setInterval(function updateTimes (){
     let timeInMinutes = Math.floor(newTime/1000/60)
 
     if (timeInMinutes < 1){
-      $timeSince[i].innerText = " - - - less than a minute ago . . . . . . . . " + moment().calendar()
+      $timeSince[i].innerText = " - - - less than a minute ago                  " + moment().calendar()
     } else if (timeInMinutes === 1) {
-      $timeSince[i].innerText = " - - - 1 minute ago . . . . . . . . " + moment().calendar()
+      $timeSince[i].innerText = " - - - 1 minute ago                " + moment().calendar()
     } else if (timeInMinutes < 60){
-      $timeSince[i].innerText = " - - - " + Math.floor(newTime/1000/60) + " minutes ago. . . . . . . . " + moment().calendar()
+      $timeSince[i].innerText = " - - - " + Math.floor(newTime/1000/60) + " minutes ago                " + moment().calendar()
     } else if (timeInMinutes >= 60 && timeInMinutes < 120){
-      $timeSince[i].innerText = " - - - 1 hour ago. . . . . . . . " + moment().calendar()
+      $timeSince[i].innerText = " - - - 1 hour ago               " + moment().calendar()
     } else {
-      $timeSince[i].innerText = " - - - " + Math.floor(newTime/1000/60/60) + " hours ago. . . . . . . . " + moment().calendar()
+      $timeSince[i].innerText = " - - - " + Math.floor(newTime/1000/60/60) + " hours ago                " + moment().calendar()
     }
 
     
@@ -493,7 +488,7 @@ setInterval(function updateTimes (){
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-$('.chop-user').css("color", "gold")
+$('.chop-user').css("color", "blue")
 
 ///////////////////////////////////////////////
 
